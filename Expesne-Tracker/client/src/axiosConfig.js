4feb1511d72b7d58e265cdba43b532da33c21003
GOATNINJA10 <148058898+KshitijBramhecha@users.noreply.github.com>
2024-01-11 16:21:03 +0530

    Delete Expesne-Tracker directory

--- Expesne-Tracker/client/src/axiosConfig.js
+++
@@ -1,8 +0,0 @@
-//axiosConfig.js
-import axios from "axios";
-
-axios.defaults.baseURL = "http://localhost:8080/";// base-url of your backend server
-
-//can also use interceptors if needed in future.
-
-export default axios;
